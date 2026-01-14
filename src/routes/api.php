0af231c6361def881fe6fd1080631294a1991ecd
gabus-baeza <ggbaeza@gmail.com>
2026-01-14 01:10:31 +0000

    Oauth

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+// Agregar el controlador EventoController
+use App\Http\Controllers\EventoController;
+/**
+* Rutas para el recurso Evento.
+*/
+// Recuperar todos los eventos
+Route::get('/eventos', [EventoController::class, 'index']);
+// Almacenar un evento nuevo
+Route::post('/eventos', [EventoController::class, 'store']);
+// Recuperar un evento específico
+Route::get('/eventos/{id}', [EventoController::class, 'show']);
+// Actualizar un evento específico
+Route::put('/eventos/{evento}', [EventoController::class, 'update']);
+// Eliminar un evento específico
+Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);

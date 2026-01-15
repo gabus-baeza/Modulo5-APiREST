5139ec4faea5d53ecbe261cbd398dcb0ba356f72
gabus-baeza <ggbaeza@gmail.com>
2026-01-15 16:18:46 +0000

    Oauth final

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,18 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-// Agregar el controlador EventoController
+
 use App\Http\Controllers\EventoController;
-/**
-* Rutas para el recurso Evento.
-*/
+
+
+/** Rutas públicas*/
 // Recuperar todos los eventos
 Route::get('/eventos', [EventoController::class, 'index']);
-// Almacenar un evento nuevo
-Route::post('/eventos', [EventoController::class, 'store']);
 // Recuperar un evento específico
 Route::get('/eventos/{id}', [EventoController::class, 'show']);
-// Actualizar un evento específico
-Route::put('/eventos/{evento}', [EventoController::class, 'update']);
-// Eliminar un evento específico
-Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
+
+
+// Rutas protegidas con autenticación Keycloak
+Route::middleware('auth:api')->group(function () {
+    // Almacenar un evento nuevo
+    Route::post('/eventos', [EventoController::class, 'store']);
+    // Actualizar un evento específico
+    Route::put('/eventos/{evento}', [EventoController::class, 'update']);
+    // Eliminar un evento específico
+    Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
+});
